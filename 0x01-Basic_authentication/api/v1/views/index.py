17fd64cee8df6c2b85b80f7efa7069438fd2fa48
Malack15 <malackokeiga@gmail.com>
2024-09-17 21:13:31 +0300

    updates

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized/', method='GET', strict_slashes=False)
+def unauthorized() -> str:
+    """
+    """
+    return abort(401)
